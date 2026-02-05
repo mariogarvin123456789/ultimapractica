8e8383e952df9a7ff3977cf7e8d580086bcd422f
mariogarvin123456789 <mariogarvin123456789@gmail.com>
2026-02-05 17:51:58 +0100

    Corrección de la lógica de caché L1 y L2

--- api/app.py
+++ api/app.py
@@ -4,23 +4,24 @@ import time
 
 app = Flask(__name__)
 
-# Conexión a Redis usando el nombre del servicio definido en docker-compose
-# Redis actúa como Caché de Nivel 2 [cite: 50, 51, 63]
 cache = redis.Redis(host='redis-server', port=6379)
 
 @app.route('/')
 def index():
-    # 1. Intentar obtener el dato de Redis (Caché L2)
+
     val = cache.get('mi_dato')
+    
     if val:
-        return f"Respuesta (HIT en L2 - Redis): {val.decode('utf-8')}"
 
-    # 2. Si no está en caché, simulamos un proceso lento de 2-3 segundos [cite: 68]
-    time.sleep(2)
+        return f"RESPUESTA: {val.decode('utf-8')} (HIT en Nivel 2 - Redis)"
+
+   
+    time.sleep(2) 
     resultado = "Información procesada por la API"
+    
 
-    # 3. Guardar el resultado en Redis para futuras peticiones
     cache.setex('mi_dato', 60, resultado)
+    
     return f"Proceso lento finalizado (MISS): {resultado}"
 
 if __name__ == "__main__":
